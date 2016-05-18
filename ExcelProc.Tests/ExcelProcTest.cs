83bfb0135fa3816f5c3e8a3d6a100302f44f0e7c
iqans <iqan.shaikh@gmail.com>
2016-05-18 12:11:20 +0100

    test methods added

--- ExcelProc.Tests/ExcelProcTest.cs
+++ ExcelProc.Tests/ExcelProcTest.cs
@@ -14,5 +14,35 @@ namespace ExcelProc.Tests
             var res = Methods.GetFinancialYearStartDate(new DateTime(2018,4,1));
             Assert.AreEqual(exp, res);
         }
+
+        [Test]
+        public void BillingPeriodShouldBe29Jun2020To24July2020WhenDateIs1July2020()
+        {
+            DateTime[] expDateTimes = new DateTime[2];
+            expDateTimes[0] = new DateTime(2020, 06, 29);
+            expDateTimes[1] = new DateTime(2020, 07, 24);
+            var res = Methods.GetBillingPeriodGeneral(new DateTime(2020, 07, 01));
+            Assert.AreEqual(expDateTimes, res);
+        }
+    }
+
+    [TestFixture]
+    public class DateRangeTest
+    {
+        [Test]
+        public void Range25May2020To30Jun2020ShouldIncludeDate15Jun2020()
+        {
+            var dr = new DateRange(new DateTime(2020, 05, 25), new DateTime(2020, 06, 30));
+            var testDate = new DateTime(2020, 06, 15);
+            Assert.IsTrue(dr.Includes(testDate));
+        }
+
+        [Test]
+        public void Range25May2020To30Jun2020ShouldIncludeDatesBetween01Jun2020To15Jun2020()
+        {
+            var dr = new DateRange(new DateTime(2020, 05, 25), new DateTime(2020, 06, 30));
+            var testDate = new DateRange(new DateTime(2020, 06, 01), new DateTime(2020, 06, 15));
+            Assert.IsTrue(dr.Includes(testDate));
+        }
     }
 }
