e0920a33b7e1dd69204527f1a880aa31203e240b
iqans <iqan.shaikh@gmail.com>
2016-05-18 10:43:47 +0100

    test project added . a simple test method added

---
+++ ExcelProc.Tests/ExcelProcTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ForecastToolMethods;
+using NUnit.Framework;
+
+namespace ExcelProc.Tests
+{
+    [TestFixture]
+    public class ExcelProcTest
+    {
+        [Test]
+        public void FinancialYearStartDateShouldBe26Mar2018ForYear2018()
+        {
+            DateTime exp = new DateTime(2018,3,26);
+            var res = Methods.GetFinancialYearStartDate(new DateTime(2018,4,1));
+            Assert.AreEqual(exp, res);
+        }
+    }
+}
